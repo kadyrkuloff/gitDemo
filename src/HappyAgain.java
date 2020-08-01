808bd10e1e54c3a5195f36baae49da082c7d8111
kadyrkuloff <kadyrkuloff@gmail.com>
2020-08-01 12:36:28 -0400

    update HappyAgain.java

--- src/HappyAgain.java
+++ src/HappyAgain.java
@@ -1,5 +1,7 @@
 public class HappyAgain {
     public static void main(String[] args) {
         System.out.println("practice");
+        boolean r = 2+2 ==4;
+        System.out.println(r);
     }
 }
