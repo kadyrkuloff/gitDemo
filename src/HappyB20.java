afee9ac2fc1ee3ecf686e6ae7c14af2efecf5a2a
kadyrkuloff <kadyrkuloff@gmail.com>
2020-08-01 11:52:14 -0400

    update HappyB20.java

--- src/HappyB20.java
+++ src/HappyB20.java
@@ -1,5 +1,8 @@
 public class HappyB20 {
     public static void main(String[] args) {
         System.out.println("Happy saturdays");
+        for (int i = 1; i <10; i++){
+            System.out.print(i +" ");
+        }
     }
 }
