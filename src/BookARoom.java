3c7bac0ab10c2eaaebfee34107a79bb789bdce1a
kadyrkuloff <68976844+kadyrkuloff@users.noreply.github.com>
2020-08-01 16:34:50 -0400

    Update BookARoom.java

--- src/BookARoom.java
+++ src/BookARoom.java
@@ -2,5 +2,6 @@ public class BookARoom {
     public static void main(String[] args) {
         System.out.println("welcome to Cybertek inn");
         System.out.println("How can we help you?");
+        System.out.println("How many night are you staying");
     }
 }
